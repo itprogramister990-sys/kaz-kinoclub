9b13b8f49663e9e085f5c52696acf2068de72d59
KinoClub Kazakhstan <kinoclub@kaz.dev>
2026-07-18 17:09:00 +0500

    Update Tauri configuration and Rust lib

--- frontend/src-tauri/src/lib.rs
+++ frontend/src-tauri/src/lib.rs
@@ -1,13 +1,19 @@
+use tauri::Manager;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
     .setup(|app| {
-      if cfg!(debug_assertions) {
+      #[cfg(debug_assertions)]
+      {
         app.handle().plugin(
           tauri_plugin_log::Builder::default()
             .level(log::LevelFilter::Info)
             .build(),
         )?;
+        // Автоматически открываем DevTools при старте (в режиме dev)
+        let window = app.get_webview_window("main").unwrap();
+        window.open_devtools();
       }
       Ok(())
     })
